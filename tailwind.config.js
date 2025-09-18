b350c53303819ea9874178a76f00be01460f066f
DammyBidi <Bidigideondamilola@gmail.com>
2025-09-18 03:25:07 +0100

    Add SVG icons and create details and index HTML pages for event platform
    - Added SVG icons for location, mail, search, twitter, and user.
    - Created details.html for event details including event description, pricing, and organizer contact.
    - Developed index.html for the main landing page featuring a hero section, trending events, and event types.
    - Configured Tailwind CSS for styling and layout.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./**/*.{html,js,ts,jsx,tsx}"] ,
+  theme: {
+    extend: {
+      fontFamily: {
+        gilroy: ['Gilroy-Bold', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
